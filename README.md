e51f2dd61696a6ef4362d91e9e61635d0a1ea412
MutheuLinet <muneenilinet@gmail.com>
2022-08-16 17:16:23 +0300

    create http server from vertx object with request handler setup

---
+++ README.md
@@ -0,0 +1,5 @@
+//start a vertx project
+mvn io.reactiverse:vertx-maven-plugin::setup
+
+//launch on localhost 8080
+mvn clean compile vertx:run
---
+++ src/main/java/com/lyn/demo/MainVerticle.java
@@ -0,0 +1,15 @@
+package com.lyn.demo;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response().end("Hello Vert.x World!");
+        }).listen(8080);
+
+    }
+
+}
